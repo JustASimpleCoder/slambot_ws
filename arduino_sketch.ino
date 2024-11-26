8a79a69eb5e3d68bb8a32c1c9ec19ceb5c32e18a
JustASimpleCoder <thejacobcohen@gmail.com>
2024-11-25 20:42:03 -0500

    added the arduibno sketch used for control

---
+++ arduino_sketch.ino
@@ -0,0 +1,142 @@
+const int right_front_pin = 2;
+const int right_front_dir1_pin = 7;
+const int right_front_dir2_pin = 6;
+
+const int right_back_pin = 3;
+const int right_back_dir1_pin = 5;
+const int right_back_dir2_pin = 4;
+
+const int left_front_pin = 11;
+const int left_front_dir1_pin = 10;
+const int left_front_dir2_pin = 8;
+
+const int left_back_pin = 9;
+const int left_back_dir1_pin = 13;
+const int left_back_dir2_pin = 12;
+
+
+int start_speed = 255;
+int motor_speed = 255;
+int count = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(right_front_pin,OUTPUT);
+  pinMode(right_front_dir1_pin,OUTPUT);
+  pinMode(right_front_dir2_pin,OUTPUT);
+  
+  pinMode(right_back_pin,OUTPUT);
+  pinMode(right_back_dir1_pin,OUTPUT);
+  pinMode(right_back_dir2_pin,OUTPUT);
+
+  pinMode(left_front_pin,OUTPUT);
+  pinMode(left_front_dir1_pin,OUTPUT);
+  pinMode(left_front_dir2_pin,OUTPUT);
+  
+  pinMode(left_back_pin,OUTPUT);
+  pinMode(left_back_dir1_pin,OUTPUT);
+  pinMode(left_back_dir2_pin,OUTPUT);
+  Serial.begin(9600);
+
+}  
+
+
+void forward_n_milisec(int n_milisec){
+   
+     digitalWrite(right_front_dir1_pin, HIGH);
+     digitalWrite(right_front_dir2_pin, LOW);
+      
+     digitalWrite(right_back_dir1_pin, LOW);
+     digitalWrite(right_back_dir2_pin, HIGH);
+      
+      
+     digitalWrite(left_front_dir1_pin, HIGH);
+     digitalWrite(left_front_dir2_pin, LOW);
+      
+     digitalWrite(left_back_dir1_pin, LOW);
+     digitalWrite(left_back_dir2_pin, HIGH);
+
+
+     digitalWrite(right_front_pin, HIGH);
+     digitalWrite(right_back_pin, HIGH);
+     digitalWrite(left_front_pin, HIGH);
+     digitalWrite(left_back_pin, HIGH);
+      
+
+//     analogWrite(right_front_pin, start_speed);
+//     analogWrite(right_back_pin, start_speed);
+//     analogWrite(left_front_pin, start_speed);
+//     analogWrite(left_back_pin, start_speed);
+//     
+//     delay(10);
+//     
+//     analogWrite(right_front_pin, motor_speed);
+//     analogWrite(right_back_pin, motor_speed);
+//     analogWrite(left_front_pin, motor_speed);
+//     analogWrite(left_back_pin, motor_speed);
+     
+     delay(n_milisec);
+     
+     stop_robot();
+     
+}
+void backward_n_milisec(int n_milisec){
+   
+     digitalWrite(right_front_dir1_pin, HIGH);
+     digitalWrite(right_front_dir2_pin, LOW);
+      
+     digitalWrite(right_back_dir1_pin, HIGH);
+     digitalWrite(right_back_dir2_pin, LOW);
+      
+      
+     digitalWrite(left_front_dir1_pin, LOW);
+     digitalWrite(left_front_dir2_pin, HIGH);
+      
+     digitalWrite(left_back_dir1_pin, LOW);
+     digitalWrite(left_back_dir2_pin, HIGH);
+      
+
+     analogWrite(right_front_pin, start_speed);
+     analogWrite(right_back_pin, start_speed);
+     analogWrite(left_front_pin, start_speed);
+     analogWrite(left_back_pin, start_speed);
+     
+     delay(10);
+     
+     analogWrite(right_front_pin, motor_speed);
+     analogWrite(right_back_pin, motor_speed);
+     analogWrite(left_front_pin, motor_speed);
+     analogWrite(left_back_pin, motor_speed);
+     
+     delay(n_milisec);
+     
+     stop_robot();
+     
+}
+
+void stop_robot(){
+     analogWrite(right_front_pin, 0);
+     analogWrite(right_back_pin, 0);
+     analogWrite(left_front_pin, 0);
+     analogWrite(left_back_pin, 0);
+}
+
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    int command = Serial.parseInt();
+    Serial.println(command);
+    switch(command){
+      case 0:
+        stop_robot();
+        break;
+      case 1:
+        forward_n_milisec(500);
+        break;
+      case 2:
+        backward_n_milisec(500);
+        break;
+    }
+  }
+}
