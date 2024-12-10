c53e801e7996db350638f27c315065d991586f21
JustASimpleCoder <thejacobcohen@gmail.com>
2024-12-10 17:53:25 -0500

    Refactored entire code, to achieve more movement control with mecanum wheel control principle

--- src/arduino_sketch/include/arduino_class.hpp
+++ src/arduino_sketch/include/arduino_class.hpp
@@ -7,6 +7,7 @@
 #define ANALOG_WRITE_WHEEL_MIN 125
 
 # define PI 3.14159265358979323846
+# define ARDUINO_SERIAL_BAUD_RATE 9600
 
 // const int STOP = 0;
 // const int MOVE_FORWARD = 1;
@@ -16,87 +17,81 @@
 // const int TURN_LEFT_OPP = 5; // both side of robot move in opposite dir  
 // const int TURN_RIGHT_OPP = 6; //both side of robot move in opposite dir  
 // const int INVALID = -1;
+
 enum class SpeedLimit {
     MIN = 125,
     MAX = 255
 };
 
-enum class RobotCommand {
-    STOP = 0,
-    MOVE_FORWARD = 1,
-    MOVE_BACKWARD = 2,
-    TURN_LEFT = 3,
-    TURN_RIGHT = 4,
-    TURN_LEFT_OPP = 5,
-    TURN_RIGHT_OPP = 6,
-    FASTER = 7,
-    SLOWER = 8,
-    INVALID = -1
+enum class RobotMovement: char{
+    STOP = 'x',
+    MOVE_FORWARD = 'w',
+    MOVE_BACKWARD = 's',
+    TURN_LEFT = 'l',
+    TURN_RIGHT = 'r',
+    TURN_LEFT_OPP = 'a',
+    TURN_RIGHT_OPP = 'd',
+    FASTER = '+',
+    SLOWER = '-',
+    INVALID = '?'
 };
 
-class Motor {
+class Motor{
+    private:
+        int m_pwm_pin;
+        int m_dir1_pin;
+        int m_dir2_pin;
     public:
-        int pwm_front_pin;
-        int dir1_front_pin;
-        int dir2_front_pin;
-
-        int pwm_back_pin;
-        int dir1_back_pin;
-        int dir2_back_pin;
-
-
-        Motor(int pwm_front, int dir1_front, int dir2_ront, int pwm_back, int dir1_back, int dir2_back)
-            : pwm_front_pin(pwm_front), dir1_front_pin(dir1_front), dir2_front_pin(dir2_ront),
-             pwm_back_pin(pwm_back), dir1_back_pin(dir1_back), dir2_back_pin(dir2_back) {}
+        Motor(int pwm, int dir1, int dir2)
+            : m_pwm_pin(pwm), m_dir1_pin(dir1), m_dir2_pin(dir2) {}
 
         void setSpeed(int speed);
         void setDirection(bool forward);
+        void pinModeSetup();
 };
 
 class MotorCommands {
-    private:
-        Motor right;
-        Motor left;
-
-        void (MotorCommands::*lastCommand)();
-        
-        bool forwards = true;
-        bool backwards = false;
-
-        //3 inches is diamter -> 0.0762 m D -> 0.0281 for radius
-        double wheel_radius = 0.0281;
-        double wheel_circum  = 2*PI*wheel_radius;
-        double wheel_base = 0.16002;
-
     public:
-        unsigned int wheel_speed;
+        unsigned int m_wheel_speed;
         MotorCommands()
-            : right(6, 4, 7, 5, 2, 3),
-            left(9, 11, 8, 10, 13, 12),
-            wheel_speed(0),
+            : right_front(5,2,3), right_back(6, 4, 7),
+            left_front(9,11,8), left_back(10,13,12),
+            m_wheel_speed(0),
             lastCommand(MotorCommands::lastCommand) {}
         
         ~MotorCommands(){
             //stopMotors();
         }
-
         void getMotorData();
         
         void getOdom();
 
         void setupArduino();
 
-        void moveForward();
+        void MotorCommands::SetMotorDir(bool left_front_wheel_forward, bool left_back_wheel_forward,
+                                bool right_front_wheel_forward, bool right_back_wheel_forward);
 
-        void moveBackward();
+        void SetMotorDirTest();
+        void SetMotorSpeed();
 
         void stopMotors();
 
-        void turnLeft();
+        void moveForward();
+        void moveBackward();
 
+
+        void turnLeft();
         void turnRight();
 
-        void loopMotorControl();
+        void moveRight();
+        void moveLeft();
+
+        void moveForwardRightDiag();
+        void moveForwardleftDiag();
+
+        void moveBackwardRightDiag();
+        void moveBackwardsleftDiag();
+        
 
         void changeSpeed(bool increase);
 
@@ -106,6 +101,24 @@ class MotorCommands {
 
         void setStartingSpeed();
 
+        void loopMotorControl();
+
         void turn(bool right);
+
+    private:
+        Motor right_back;
+        Motor right_front;
+        Motor left_front;
+        Motor left_back;
+
+        void (MotorCommands::*lastCommand)();
+        
+        bool m_forwards = true;
+        bool m_backwards = false;
+
+        //3 inches is diamter -> 0.0762 m D -> 0.0281 for radius
+        double m_wheel_radius = 0.0281;
+        double m_wheel_circum  = 2*PI*m_wheel_radius;
+        double m_wheel_base = 0.16002;
 };
 #endif
